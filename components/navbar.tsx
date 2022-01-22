f103b8265e91c493b1d53c3e38811f9bf454e88b
Nico Ismaili <nico@ismaili.de>
2022-01-22 22:44:38 +0100

    fix: mobile version

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,7 +1,8 @@
 import Image from "next/image";
 import React, { Component } from "react";
-import Row from "react-bootstrap/Row";
-import Col from "react-bootstrap/Col";
+import Navbar from "react-bootstrap/Navbar";
+import Nav from "react-bootstrap/Nav";
+import Container from "react-bootstrap/Container";
 
 interface Props {}
 
@@ -12,18 +13,55 @@ const links = ["#about", "#projects", "#contact"];
 export default class NavBar extends Component<Props, State> {
   render() {
     return (
-      <Row className="p-5">
-        <Col xs={6}>
-          <a href="#home">
-            <Image src="/images/emblem.png" height={50} width={50} />
-          </a>
-        </Col>
-        <Col xs={6} className="text-end pt-1">
-          {pages.map((page, index) => (
-            <a href={links[index]} className="hover-line mx-3">{page.toString()}</a>
-          ))}
-        </Col>
-      </Row>
+      <div className="sticky-top px-lg-5 pt-lg-3 px-md-4 ps-4 pe-3 pt-2">
+        <Navbar collapseOnSelect bg="white" expand="md">
+            <Navbar.Brand href="#home">
+              <Image
+                className="mw-100"
+                src="/images/emblem.png"
+                height={50}
+                width={50}
+              />
+            </Navbar.Brand>
+            <Navbar.Toggle className="border-0 pb-1" aria-controls="responsive-navbar-nav" />
+            <Navbar.Collapse id="responsive-navbar-nav">
+              <Nav className="me-auto"></Nav>
+              <Nav defaultActiveKey="#home">
+                {pages.map((page, index) => (
+                  <Nav.Link className="themed-nav p-0 mx-3 pb-1 mt-1" key={"nav-" + index} href={links[index]}>
+                    {page.toString()}
+                  </Nav.Link>
+                ))}
+              </Nav>
+            </Navbar.Collapse>
+        </Navbar>
+      </div>
     );
   }
 }
+
+/*
+ <div className="navigation navbar-expand-lg d-flex justify-content-between">
+        <div className="container-fluid">
+          <a className="navbar-brand" href="#home">
+            
+          </a>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarNav"
+            aria-controls="navbarNav"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse">
+            <ul className="navbar-nav">
+              
+            </ul>
+          </div>
+        </div>
+      </div>
+*/
--- pages/index.tsx
+++ pages/index.tsx
@@ -12,22 +12,21 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <NavBar />
-    <Container className="m-0 ms-4 mt-5 ">
+    <Container className="mt-lg-5 pt-lg-5 mx-lg-5 mx-2">
         <Row>
           <Col>
-            <h1>
+            <h1 className="display-1 mt-md-5 pt-md-5 pt-4">
               Hi, ich bin
               <br />
-              <span className="text-primary">Nico</span> Ismaili.
+              <span className="text-primary display-1">Nico</span> Ismaili.
             </h1>
           </Col>
         </Row>
         <Row>
           <Col>
-            <h2>
-              Ein <span className="text-primary">Full-Stack Entwickler</span>{" "}
-              aus
-              <br />
+            <h2 className="display-5">
+              Ein <span className="text-primary display-5">Full-Stack Entwickler</span>{" "}
+              aus <br className="d-none d-lg-block"/>
               Wiesbaden, Deutschland.
             </h2>
           </Col>
