9d7827b7f7d3c489bd6894fd814b0126c83927a0
Nico Ismaili <nico@ismaili.de>
2022-01-22 13:45:45 +0100

    add: project pages placeholder

---
+++ pages/projects/[id].tsx
@@ -0,0 +1,12 @@
+import Head from 'next/head'
+
+export default function Project() {
+  return (
+    <div>
+      <Head>
+        <title>Next.js Starter!</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+    </div>
+  )
+}
