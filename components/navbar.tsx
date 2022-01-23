30e51e8730d118d2626da17cb0a10932976d58d3
Nico Ismaili <nico@ismaili.de>
2022-01-23 09:59:37 +0100

    fix: mobile menu

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -2,6 +2,7 @@ import Image from "next/image";
 import React, { Component } from "react";
 import Navbar from "react-bootstrap/Navbar";
 import Nav from "react-bootstrap/Nav";
+import Offcanvas from "react-bootstrap/Offcanvas";
 
 interface Props {}
 
@@ -13,54 +14,65 @@ export default class NavBar extends Component<Props, State> {
   render() {
     return (
       <div className="sticky-top px-lg-5 pt-lg-3 px-md-4 px-3 pt-2">
-        <Navbar collapseOnSelect bg="transparent" expand="md">
-            <Navbar.Brand href="#home">
-              <Image
-                className="mw-100"
-                src="/images/emblem.png"
-                height={50}
-                width={50}
-              />
-            </Navbar.Brand>
-            <Navbar.Toggle className="border-0 pb-1" aria-controls="responsive-navbar-nav" />
-            <Navbar.Collapse id="responsive-navbar-nav">
-              <Nav className="me-auto"></Nav>
-              <Nav defaultActiveKey="#home">
+        <Navbar collapseOnSelect expand="md">
+          <Navbar.Brand href="#home">
+            <Image
+              className="mw-100"
+              src="/images/emblem.png"
+              height={50}
+              width={50}
+            />
+          </Navbar.Brand>
+          <Navbar.Toggle
+            className="border-0 pb-1"
+            aria-controls="offcanvasNavbar"
+          />
+          <div className="d-none d-md-block">
+          <Nav className="me-auto"></Nav>
+          <Nav defaultActiveKey="#home">
+            {pages.map((page, index) => (
+              <Nav.Link
+                className="themed-nav p-0 mx-3 pb-1 mt-1"
+                key={"nav-" + index}
+                href={links[index]}
+              >
+                {page.toString()}
+              </Nav.Link>
+            ))}
+          </Nav>
+          </div>
+          <Navbar.Offcanvas
+            id="offcanvasNavbar"
+            aria-labelledby="offcanvasNavbarLabel"
+            placement="end"
+          >
+            <Offcanvas.Header className="pb-0" closeButton>
+              <Offcanvas.Title id="offcanvasNavbarLabel">
+                <Image
+                  className="mw-100"
+                  src="/images/emblem.png"
+                  height={50}
+                  width={50}
+                />
+              </Offcanvas.Title>
+            </Offcanvas.Header>
+
+            <Offcanvas.Body>
+              <Nav className="flex-grow-1 d-sm-none">
                 {pages.map((page, index) => (
-                  <Nav.Link className="themed-nav p-0 mx-3 pb-1 mt-1" key={"nav-" + index} href={links[index]}>
+                  <Nav.Link
+                    className="themed-nav p-0 mx-3 pb-1 mt-1"
+                    key={"nav-" + index}
+                    href={links[index]}
+                  >
                     {page.toString()}
                   </Nav.Link>
                 ))}
               </Nav>
-            </Navbar.Collapse>
+            </Offcanvas.Body>
+          </Navbar.Offcanvas>
         </Navbar>
       </div>
     );
   }
 }
-
-/*
- <div className="navigation navbar-expand-lg d-flex justify-content-between">
-        <div className="container-fluid">
-          <a className="navbar-brand" href="#home">
-            
-          </a>
-          <button
-            className="navbar-toggler"
-            type="button"
-            data-bs-toggle="collapse"
-            data-bs-target="#navbarNav"
-            aria-controls="navbarNav"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
-          >
-            <span className="navbar-toggler-icon"></span>
-          </button>
-          <div className="collapse navbar-collapse">
-            <ul className="navbar-nav">
-              
-            </ul>
-          </div>
-        </div>
-      </div>
-*/
