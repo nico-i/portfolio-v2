44910427dd62386cc2b2045f637bd78fc9799384
Nico Ismaili <nico@ismaili.de>
2022-02-16 17:33:34 +0100

    updated: purge css

--- next.config.js
+++ next.config.js
@@ -1,9 +1,29 @@
-const withCss = require("@zeit/next-css");
-const withPurgeCss = require("next-purgecss");
-
-module.exports = withCss(withPurgeCss());
-module.exports += {
-
+module.exports = {
+    "plugins": [
+        "postcss-flexbugs-fixes", [
+            "postcss-preset-env",
+            {
+                "autoprefixer": {
+                    "flexbox": "no-2009"
+                },
+                "stage": 3,
+                "features": {
+                    "custom-properties": false
+                }
+            }
+        ],
+        [
+            '@fullhuman/postcss-purgecss',
+            {
+                content: [
+                    './pages/**/*.{js,jsx,ts,tsx}',
+                    './components/**/*.{js,jsx,ts,tsx}'
+                ],
+                defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || [],
+                safelist: ["html", "body"]
+            }
+        ],
+    ],
     images: {
         domains: ['nico-ismaili.netlify.app'],
         basepath: '/images',
