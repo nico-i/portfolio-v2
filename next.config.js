0761db8a88f756e7f87b0d179780828745089c17
Nico Ismaili <nico@ismaili.de>
2022-02-16 14:59:22 +0100

    add: css purge

--- next.config.js
+++ next.config.js
@@ -1,4 +1,9 @@
-module.exports = {
+const withCss = require("@zeit/next-css");
+const withPurgeCss = require("next-purgecss");
+
+module.exports = withCss(withPurgeCss());
+module.exports += {
+
     images: {
         domains: ['nico-ismaili.netlify.app'],
         basepath: '/images',
