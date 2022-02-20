d01921a1d82b183fd65cadb842ad364462144b3d
Nico Ismaili <nico@ismaili.de>
2022-02-20 14:46:24 +0100

    add: react intersection observer to only load skills when visible

--- components/skill.tsx
+++ components/skill.tsx
@@ -1,5 +1,5 @@
 import { easeQuadInOut } from "d3-ease";
-import React, { Component } from "react";
+import React from "react";
 import {
   buildStyles,
   CircularProgressbarWithChildren,
@@ -17,76 +17,58 @@ interface Props {
   pathColor: string;
   repeat?: boolean;
   icon: string;
+  isInView: boolean;
 }
 
-interface State {
-  isAnimated: boolean;
-}
-
-export default class Skill extends Component<Props, State> {
-  constructor(props: Props) {
-    super(props);
-    this.state = { isAnimated: false };
-  }
-
-  componentDidMount() {
-    const duration = this.props.duration ? this.props.duration : 1.4;
-    const repeat = this.props.repeat ? this.props.repeat : false;
-    if (repeat) {
-      window.setInterval(() => {
-        this.setState({
-          isAnimated: !this.state.isAnimated,
-        });
-      }, duration * 1000);
-    } else {
-      this.setState({
-        isAnimated: !this.state.isAnimated,
-      });
-    }
-  }
-  render() {
-    const { skill, width, endPercentage, backgroundColor, pathColor, icon } =
-      this.props;
-    const startPercentage = this.props.startPercentage
-      ? this.props.startPercentage
-      : 0;
-    const duration = this.props.duration ? this.props.duration : 1.4;
-    return (
-      <>
-        <div style={{ width: width }}>
-          <Animate
-            start={() => ({
-              value: startPercentage,
-            })}
-            update={() => ({
-              value: [this.state.isAnimated ? endPercentage : startPercentage],
-              timing: {
-                duration: (duration ? duration : 1.4) * 1000,
-                ease: easeQuadInOut,
-              },
-            })}
-          >
-            {({ value }) => {
-              return (
-                <CircularProgressbarWithChildren
-                  key={skill}
-                  background
-                  backgroundPadding={6}
-                  value={value}
-                  styles={buildStyles({
-                    pathTransition: "none",
-                    backgroundColor: backgroundColor,
-                    pathColor: pathColor,
-                    trailColor: "transparent",
-                  })}
-                >
-                  <DynamicIcon name={icon} fill={pathColor} />
-                </CircularProgressbarWithChildren>
-              );
-            }}
-          </Animate>
-        </div>
-      </>
-    );
-  }
+export default function Skill(props: Props) {
+  const {
+    skill,
+    width,
+    endPercentage,
+    backgroundColor,
+    pathColor,
+    icon,
+    isInView,
+  } = props;
+  const startPercentage = props.startPercentage
+    ? props.startPercentage
+    : 0;
+  const duration = props.duration ? props.duration : 1.4;
+  return (
+    <>
+      <div style={{ width: width }}>
+        <Animate
+          start={() => ({
+            value: startPercentage,
+          })}
+          update={() => ({
+            value: [isInView ? endPercentage : startPercentage],
+            timing: {
+              duration: (duration ? duration : 1.4) * 1000,
+              ease: easeQuadInOut,
+            },
+          })}
+        >
+          {({ value }) => {
+            return (
+              <CircularProgressbarWithChildren
+                key={skill}
+                background
+                backgroundPadding={6}
+                value={value}
+                styles={buildStyles({
+                  pathTransition: "none",
+                  backgroundColor: backgroundColor,
+                  pathColor: pathColor,
+                  trailColor: "transparent",
+                })}
+              >
+                <DynamicIcon name={icon} fill={pathColor} />
+              </CircularProgressbarWithChildren>
+            );
+          }}
+        </Animate>
+      </div>
+    </>
+  );
 }
