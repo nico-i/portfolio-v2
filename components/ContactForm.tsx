8aef6c99d57f17dda8d9128f0f36c9d2d9abd48a
Nico Ismaili <nico@ismaili.de>
2022-02-27 21:47:24 +0100

    add: placeholder comp files

---
+++ components/ContactForm.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+interface Props {
+    budgetOptions: string[];
+}
+
+//TODO Create contact form
+
+export default function ContactForm(props: Props) {
+  
+  return (
+    <>
+
+    </>
+  );
+}
---
+++ components/SkillsWrapper.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+interface Props {
+}
+
+//TODO Create skillwrapper which shows information about skill when hovered over
+
+export default function ContactForm(props: Props) {
+  
+  return (
+    <>
+
+    </>
+  );
+}
