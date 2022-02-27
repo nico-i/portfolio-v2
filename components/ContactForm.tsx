b72117d6c32703a6b639fca921c12150f1c80478
Nico Ismaili <nico@ismaili.de>
2022-02-27 22:22:33 +0100

    fix: imports

--- components/ContactForm.tsx
+++ components/ContactForm.tsx
@@ -1,8 +1,7 @@
 import React from "react";
 import Col from "react-bootstrap/Col";
-import Container from "react-bootstrap/Container";
-import Row from "react-bootstrap/Row";
 import Form from "react-bootstrap/Form";
+import Row from "react-bootstrap/Row";
 interface Props {
   //budgetOptions: string[];
 }
@@ -11,76 +10,76 @@ interface Props {
 
 export default function ContactForm(props: Props) {
   return (
-      <Form>
-        <Row>
-          <Col xs={4}>
-            <h2 className="text-start">
-              Lassen Sie
-              <br />
-              uns etwas
-              <span className="text--primary"> großartiges</span> zusammen
-              schaffen.
-            </h2>
-            <Form.Group
-              className="mb-3 w-100"
-              controlId="exampleForm.ControlInput1"
-            >
-              <Form.Label>Budget</Form.Label>
-              <Form.Select aria-label="Default select example">
-                <option>Open this select menu</option>
-                <option value="1">One</option>
-                <option value="2">Two</option>
-                <option value="3">Three</option>
-              </Form.Select>
-            </Form.Group>
-            <Form.Group
-              className="mb-3 w-100"
-              controlId="exampleForm.ControlInput1"
-            >
-              <Form.Label>Vor- & Nachname</Form.Label>
-              <Form.Control type="text" placeholder="Max Mustermann" />
-            </Form.Group>
-            <Form.Group
-              className="mb-3 w-100"
-              controlId="exampleForm.ControlInput1"
-            >
-              <Form.Label>Email address</Form.Label>
-              <Form.Control type="email" placeholder="name@example.com" />
-            </Form.Group>
-          </Col>
-          <Col xs={8}>
-            <Form.Group
-              className="mb-3 w-100"
-              controlId="exampleForm.ControlTextarea1"
-            >
-              <Form.Label>Nachricht</Form.Label>
-              <Form.Control as="textarea" rows={3} />
-            </Form.Group>
-            <Row>
-              <Col md={3}>
-                <p>Social Media</p>
-              </Col>
-              <Col md={3}>
-                <p>Social Media</p>
-              </Col>
-              <Col md={3}>
-                <p>Social Media</p>
-              </Col>
-              <Col md={3}>
-                <button>Test</button>
-              </Col>
-              <Col md={3}>
-                <p>Social Media</p>
-              </Col>
-              <Col md={3}>
-                <p>Social Media</p>
-              </Col>
-              <Col md={3}>
-                <p>Social Media</p>
-              </Col>
-            </Row>
-          </Col>
-        </Row>
-      </Form>
+    <Form>
+      <Row>
+        <Col xs={4}>
+          <h2 className="text-start">
+            Lassen Sie
+            <br />
+            uns etwas
+            <span className="text--primary"> großartiges</span> zusammen
+            schaffen.
+          </h2>
+          <Form.Group
+            className="mb-3 w-100"
+            controlId="exampleForm.ControlInput1"
+          >
+            <Form.Label>Budget</Form.Label>
+            <Form.Select aria-label="Default select example">
+              <option>Open this select menu</option>
+              <option value="1">One</option>
+              <option value="2">Two</option>
+              <option value="3">Three</option>
+            </Form.Select>
+          </Form.Group>
+          <Form.Group
+            className="mb-3 w-100"
+            controlId="exampleForm.ControlInput1"
+          >
+            <Form.Label>Vor- & Nachname</Form.Label>
+            <Form.Control type="text" placeholder="Max Mustermann" />
+          </Form.Group>
+          <Form.Group
+            className="mb-3 w-100"
+            controlId="exampleForm.ControlInput1"
+          >
+            <Form.Label>Email address</Form.Label>
+            <Form.Control type="email" placeholder="name@example.com" />
+          </Form.Group>
+        </Col>
+        <Col xs={8}>
+          <Form.Group
+            className="mb-3 w-100"
+            controlId="exampleForm.ControlTextarea1"
+          >
+            <Form.Label>Nachricht</Form.Label>
+            <Form.Control as="textarea" rows={3} />
+          </Form.Group>
+          <Row>
+            <Col md={3}>
+              <p>Social Media</p>
+            </Col>
+            <Col md={3}>
+              <p>Social Media</p>
+            </Col>
+            <Col md={3}>
+              <p>Social Media</p>
+            </Col>
+            <Col md={3}>
+              <button>Test</button>
+            </Col>
+            <Col md={3}>
+              <p>Social Media</p>
+            </Col>
+            <Col md={3}>
+              <p>Social Media</p>
+            </Col>
+            <Col md={3}>
+              <p>Social Media</p>
+            </Col>
+          </Row>
+        </Col>
+      </Row>
+    </Form>
   );
 }
