64bb082252229d6c3c09955ace98b6897cd9496f
Nico Ismaili <nico@ismaili.de>
2022-04-02 13:33:56 +0200

    💄 Added scroll snapping

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,29 +4,32 @@ import { useInView } from "react-intersection-observer";
 import SkillCircle from "../components/SkillCircle/SkillCircle";
 import Skills from "../data/Skills";
 import TypreWriter from "typewriter-effect";
+import styles from "./index.module.css";
 
 const Home: NextPage = () => {
   const { ref, inView } = useInView({
     threshold: 0,
   });
   return (
-    <div>
-      <h1>
-        <TypreWriter
-          options={{
-            strings: [
-              "Hi.",
-              'Mein Name ist<br/><span class="text-primary">Nico</span> Ismaili.',
-              'Ich bin ein<span class="d-none d-lg-inline"> </span><br class="d-block d-lg-none"/><span class="text-primary">Full-Stack<br />Entwickler</span> aus<span class="d-none d-md-inline"> </span><br class="d-block d-md-none"/>Wiesbaden, <br /> Deutsch&shyland.',
-              'Willkommen <br class="d-lg-none d-block"/>zu <br class="d-none d-lg-block"/>meinem <br class="d-lg-none d-block"/><span class="text-primary">Portfolio.</span>',
-            ],
-            autoStart: true,
-            loop: true,
-            delay: Math.floor(Math.random() * (130 - 90 + 1)) + 90,
-          }}
-        />
-      </h1>
-      <div ref={ref}>
+    <div className={styles.siteWrapper}>
+      <section className={styles.section}>
+        <h1 className={styles.typedHero}>
+          <TypreWriter
+            options={{
+              strings: [
+                "Hi.",
+                'Mein Name ist <span class="text-primary">Nico</span> Ismaili.',
+                'Ich bin ein <span class="text-primary">Full-Stack<br />Entwickler</span> aus Wiesbaden, Deutsch&shyland.',
+                'Willkommen zu meinem <span class="text-primary">Portfolio.</span>',
+              ],
+              autoStart: true,
+              loop: true,
+              delay: Math.floor(Math.random() * (130 - 90 + 1)) + 90,
+            }}
+          />
+        </h1>
+      </section>
+      <section className={styles.section} ref={ref}>
         {Skills.map((entry) => (
           <SkillCircle
             key={entry.skill}
@@ -39,7 +42,7 @@ const Home: NextPage = () => {
             Icon={entry.Icon}
           />
         ))}
-      </div>
+      </section>
     </div>
   );
 };
