7078488384e2914d88767fb43102390f8a8e4678
Nico Ismaili <nico@ismaili.de>
2022-04-02 13:33:27 +0200

    🔧 Fixed tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,13 @@
 module.exports = {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx} ",
+    "./components/**/*.{js,ts,jsx,tsx} ",
   ],
-
   theme: {
-    extend: {
-      fontFamily: {
-        sans: ["Poppins", "sans-serif"],
-      },
+    fontFamily: {
+      sans: ["Poppins ", "sans-serif "],
     },
   },
+  extend: {},
   plugins: [],
 };
