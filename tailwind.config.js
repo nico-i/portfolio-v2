9c8f6226a0eb45b177e988039705f0aca3e3ee56
Nico Ismaili <nico@ismaili.de>
2022-04-02 20:37:26 +0200

    💄 Add theme colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,14 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx} ",
   ],
   theme: {
+    colors: {
+      transparent: "transparent",
+      current: "currentColor",
+      white: "#ffffff",
+      primary: "#007dff",
+      light: "#f7f7f7",
+      dark: "#262626",
+    },
     fontFamily: {
       sans: ["Poppins ", "sans-serif "],
     },
