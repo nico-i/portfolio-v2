25a37c1c09fd5744c91d23199063d7aa4ae78028
Nico Ismaili <nico@ismaili.de>
2022-04-03 23:27:47 +0200

    🔧 Added css module compatibility with tailwind's 'dark:'

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,6 @@
-import React from "react";
+import { ThemeProvider } from "next-themes";
 import { AppProps } from "next/app";
+import React from "react";
 import "../styles/globals.css";
 
 /**
@@ -7,12 +8,12 @@ import "../styles/globals.css";
  * @param {AppProps} param0
  * @return {React.ReactNode}
  */
-function Application({ Component, pageProps }: AppProps) {
+function PortfolioApp({ Component, pageProps }: AppProps) {
   return (
-    <>
+    <ThemeProvider attribute="class">
       <Component {...pageProps} />
-    </>
+    </ThemeProvider>
   );
 }
 
-export default Application;
+export default PortfolioApp;
