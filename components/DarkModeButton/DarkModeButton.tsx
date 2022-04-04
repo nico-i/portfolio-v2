da7b386d32843f4da6359d12a939aab9ea776a82
Nico Ismaili <nico@ismaili.de>
2022-04-05 00:13:57 +0200

    ✨ Made darkmode button its own component

---
+++ components/DarkModeButton/DarkModeButton.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+import { RiMoonFill, RiSunFill } from "react-icons/ri";
+import styles from "./DarkModeButton.module.css";
+
+interface Props {
+  onThemeChange: Function;
+  theme: string;
+}
+
+/**
+ *
+ * @return {React.ReactNode}
+ */
+export default function DarkModeButton({ onThemeChange, theme }: Props) {
+  return (
+    <button
+      aria-label="Toggle Dark Mode"
+      type="button"
+      onClick={() => onThemeChange(theme === "dark" ? "light" : "dark")}
+    >
+      <div className={styles.themeToggle}>
+        {theme === "dark" ? <RiSunFill /> : <RiMoonFill />}
+      </div>
+    </button>
+  );
+}
--- components/NavBar/NavBar.tsx
+++ components/NavBar/NavBar.tsx
@@ -2,8 +2,8 @@ import Image from "next/image";
 import React from "react";
 import darkLogo from "../../public/images/emblem_dark.svg";
 import lightLogo from "../../public/images/emblem_light.svg";
+import DarkModeButton from "../DarkModeButton/DarkModeButton";
 import styles from "./NavBar.module.css";
-import { RiMoonFill, RiSunFill } from "react-icons/ri";
 
 interface Props {
   onThemeChange: Function;
@@ -24,16 +24,7 @@ export default function NavBar({ onThemeChange, theme }: Props) {
           height={50}
         />
       </a>
-      <button
-        aria-label="Toggle Dark Mode"
-        type="button"
-        className="p-3 h-12 w-12 dark:text-light text-dark"
-        onClick={() => onThemeChange(theme === "dark" ? "light" : "dark")}
-      >
-        <div className={styles.themeToggle}>
-          {theme === "dark" ? <RiSunFill /> : <RiMoonFill />}
-        </div>
-      </button>
+      <DarkModeButton onThemeChange={onThemeChange} theme={theme} />
     </header>
   );
 }
