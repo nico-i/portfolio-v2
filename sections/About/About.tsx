9ef3a89646186687ac8f55a3394910151c5713c0
Nico Ismaili <nico@ismaili.de>
2022-04-05 14:30:16 +0200

    💬 Updated links

--- sections/About/About.tsx
+++ sections/About/About.tsx
@@ -20,29 +20,40 @@ export default function About() {
       </div>
       <div className={styles.aboutText}>
         <h2>
-          Darf ich mich <span className="highlighted">vorstellen?</span>
+          Darf ich mich <span className="highlighted">vorstellen</span>?
         </h2>
         <p>
           Wie bereits erwähnt heiße ich Nico. Ich studiere{" "}
-          <a className="link" href="#">
+          <a
+            href="https://www.hs-rm.de/de/fachbereiche/design-informatik-medien/studiengaenge/medieninformatik-bsc"
+            target="_blank"
+            rel="noreferrer"
+          >
             Medieninformatik
           </a>{" "}
           im 5. Semester an der{" "}
-          <a className="link--underlined" href="#">
+          <a href="https://www.hs-rm.de" target="_blank" rel="noreferrer">
             Hochschule RheinMain
           </a>{" "}
           in Wiesbaden.
         </p>
         <p>
           Parallel zu meinem Studium arbeite ich als Werkstudent bei{" "}
-          <a className="link--underlined" href="#">
+          <a href="https://www.forsuxess.de/" target="_blank" rel="noreferrer">
             forsuxess
           </a>
           , ein Unternehmen dass sich auf HR-IT spezialisiert.
         </p>
         <p>
-          In meiner Freizeit widme ich mich der <a href="#">Fotografie</a> oder
-          halte mich auf dem Laufenden was die neueste Technik angeht.
+          In meiner Freizeit widme ich mich der{" "}
+          <a
+            href="https://500px.com/p/nicoismaili?view=photos"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Fotografie
+          </a>{" "}
+          oder halte mich auf dem Laufenden was die neueste Technik angeht.
         </p>
       </div>
     </div>
