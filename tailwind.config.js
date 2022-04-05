4ce2311294e5fdb765c07000b0b580eacd577268
Nico Ismaili <nico@ismaili.de>
2022-04-05 23:38:22 +0200

    ➕ Added tailwind forms plugin

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,16 +9,17 @@ module.exports = {
     colors: {
       transparent: "transparent",
       current: "currentColor",
-      white: "#ffffff",
       primary: "#007dff",
       primary_dark: "#2890FF",
       light: "#f7f7f7",
+      lighter: "#fcfcfc",
       dark: "#262626",
+      darker: "#1a1a1a",
     },
     fontFamily: {
       sans: ["Poppins ", "sans-serif "],
     },
   },
   extend: {},
-  plugins: [],
+  plugins: [require("@tailwindcss/forms")],
 };
