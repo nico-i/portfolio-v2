2f05e0c736cbdf68f47e4046a59d7aae5b18abc4
Nico Ismaili <nico@ismaili.de>
2022-04-07 20:34:56 +0200

    🔧 Update config

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -5,6 +5,8 @@ module.exports = {
     defaultLocale: "en-US",
     locales: ["en-US", "de-DE"],
   },
+  ns: ["common", "about", "contact", "hero", "nav"],
+  defaultNS: "common",
   ...(typeof window === undefined
     ? { localePath: path.resolve("./public/locales") }
     : {}),
