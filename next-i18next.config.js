08c21abb440f8b4c95f2f13c9b656be7261ac45b
Nico Ismaili <nico@ismaili.de>
2022-04-07 19:20:04 +0200

    🔧 Updated config

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -1,9 +1,6 @@
-const path = require("path");
-
 module.exports = {
   i18n: {
     defaultLocale: "en-US",
     locales: ["en-US", "de-DE"],
   },
-  localePath: path.resolve("./public/locales"),
 };
