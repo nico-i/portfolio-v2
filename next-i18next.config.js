2d3e10b54a428af6d4168b759ea008ae347a0daf
Nico Ismaili <nico@ismaili.de>
2022-04-07 12:40:52 +0200

    🔧 Updated config to enable netlify build

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -1,6 +1,9 @@
+const path = require("path");
+
 module.exports = {
   i18n: {
     defaultLocale: "en-US",
     locales: ["en-US", "de-DE"],
+    localePath: path.resolve("./public/locales"),
   },
 };
