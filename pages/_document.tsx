a21fa61b7b2347424ef7a23b58418a12fe011da3
Nico Ismaili <nico@ismaili.de>
2023-07-06 23:40:54 +0200

    ⬆️ Updated deps

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -8,16 +8,6 @@ export default function Document() {
   return (
     <Html>
       <Head>
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link
-          rel="preconnect"
-          href="https://fonts.gstatic.com"
-          crossOrigin="true"
-        />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Poppins:wght@500;600&display=swap"
-          rel="stylesheet"
-        />
         <link rel="shortcut icon" href="/images/favicon/favicon.ico" />
         <link
           rel="icon"
